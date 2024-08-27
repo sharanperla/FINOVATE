e1a323df380f1839aff81a9baf628aba12c4afbb
sharan kumar <sharanperla1234@gmail.com>
2024-08-27 21:44:32 +0530

    path issue resolved

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use('/api/transactions',transactionsRouter);
 app.use(express.static(path.join(__dirname,'../client/dist')))
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'../cleint','dist','index.html'))
+    res.sendFile(path.join(__dirname,'../client','dist','index.html'))
 })
 
 app.listen(3000,()=>{
